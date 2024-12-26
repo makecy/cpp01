d60247ff8a539e917f05153f7cecb91ef60b6ec2
mstefano <mstefano@student.42heilbronn.de>
2024-12-27 00:59:30 +0100

    removed outputs for heap zombie and stack zombie addition (useless)

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -2,13 +2,9 @@
 
 int main ()
 {
-	std::cout << "Creating a Zombie on the heap:" << std::endl;
 	Zombie* heapZombie = newZombie("HeapZombie");
 	heapZombie->announce();
 	delete heapZombie;
-	
-	std::cout << "Creating a Zombie on the Stack" << std::endl;
 	randomChump("StackZombie");
-	
 	return 0;
 }
